30594c59fb13de05751f6578e84bbd030f0fe1dd
ciplex <masterkemseng@yahoo.com>
2018-03-05 08:45:35 +0700

    updates

--- js/main.js
+++ js/main.js
@@ -636,6 +636,21 @@ $(document).ready(function() {
  });
 
 
+ $(".custom-file-upload").upload({
+    action: "phpexcel.php",
+    beforeSend: onBeforeSend
+  });
+  function onBeforeSend(formData, file) {
+    // Cancel request
+    if (file.name.indexOf(".jpg") < 0) {
+      return false;
+    }
+  
+    // Modify and return form data
+    formdata.append("input_name", "input_value");
+  
+    return formData;
+  }
 
 // Fungsi Untuk Upload File
 // $(function(){
