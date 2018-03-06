16923151432c3adf20705790398ac1a05e600626
ciplex <masterkemseng@yahoo.com>
2018-03-06 21:20:41 +0700

    updates

--- php/phpexcel.php
+++ php/phpexcel.php
@@ -34,4 +34,77 @@ header('Cache-Control: max-age=0');
 $file = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
 // Output to php output
 $file->save('php://output');
+
+$errors = array();
+$uploadedFiles = array();
+$extension = array("jpeg","jpg","png","gif");
+$bytes = 1024;
+$KB = 1024;
+$totalBytes = $bytes * $KB;
+$UploadFolder = "UploadFolder";
+ 
+$counter = 0;
+ 
+foreach($_FILES["files"]["tmp_name"] as $key=>$tmp_name){
+    $temp = $_FILES["files"]["tmp_name"][$key];
+    $name = $_FILES["files"]["name"][$key];
+     
+    if(empty($temp))
+    {
+        break;
+    }
+     
+    $counter++;
+    $UploadOk = true;
+     
+    if($_FILES["files"]["size"][$key] > $totalBytes)
+    {
+        $UploadOk = false;
+        array_push($errors, $name." file size is larger than the 1 MB.");
+    }
+     
+    $ext = pathinfo($name, PATHINFO_EXTENSION);
+    if(in_array($ext, $extension) == false){
+        $UploadOk = false;
+        array_push($errors, $name." is invalid file type.");
+    }
+     
+    if(file_exists($UploadFolder."/".$name) == true){
+        $UploadOk = false;
+        array_push($errors, $name." file is already exist.");
+    }
+     
+    if($UploadOk == true){
+        move_uploaded_file($temp,$UploadFolder."/".$name);
+        array_push($uploadedFiles, $name);
+    }
+}
+ 
+if($counter>0){
+    if(count($errors)>0)
+    {
+        echo "<b>Errors:</b>";
+        echo "<br/><ul>";
+        foreach($errors as $error)
+        {
+            echo "<li>".$error."</li>";
+        }
+        echo "</ul><br/>";
+    }
+     
+    if(count($uploadedFiles)>0){
+        echo "<b>Uploaded Files:</b>";
+        echo "<br/><ul>";
+        foreach($uploadedFiles as $fileName)
+        {
+            echo "<li>".$fileName."</li>";
+        }
+        echo "</ul><br/>";
+         
+        echo count($uploadedFiles)." file(s) are successfully uploaded.";
+    }                               
+}
+else{
+    echo "Please, Select file(s) to upload.";
+}
 ?>
